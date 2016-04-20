3087c40f5c3ad3d142c270e9300d6833e70ec734
cesaracastro18 <cesaracastro18@outlook.com>
2016-04-20 08:44:39 -0500

    proyecto 3
    proyecto 3 next u, en android studio

---
+++ app/src/main/java/tryjrt/com/micrmcurso3/apprisa/micrm/Actividades.java
@@ -0,0 +1,26 @@
+package tryjrt.com.micrmcurso3.apprisa.micrm;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Actividades extends Fragment {
+
+
+    public Actividades() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_actividades, container, false);
+    }
+
+}
